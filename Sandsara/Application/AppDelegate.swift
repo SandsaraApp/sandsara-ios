8e91277ed0a740105fc12b52686d59ae3aaba979
Tin <tin@iwa.fi>
2021-03-11 14:47:50 +0700

    fix bluetooth error

--- Sandsara/Application/AppDelegate.swift
+++ Sandsara/Application/AppDelegate.swift
@@ -62,7 +62,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
         
-        if (UIApplication.topViewController()?.tabBarController?.popupBar.customBarViewController as? PlayerBarViewController)?.state == .busy {
+        if (UIApplication.topViewController() is OverlaySendFileViewController) {
             isFromBackgroundResume = false
         } else {
             isFromBackgroundResume = true
